8e363aa89ecb57521bdf7a446bd62d1f293805b4
iainhemstock <iainhemstock@googlemail.com>
2016-08-20 15:46:02 +0100

    commiting

---
+++ inc/prism/Pair.h
@@ -0,0 +1,22 @@
+/*
+ * Pair.h
+ * v1
+ *
+ *  Created on: Aug 20, 2016
+ *      Author: iainhemstock
+ */
+
+#ifndef PRISM_PAIR_H_
+#define PRISM_PAIR_H_
+
+namespace prism {
+
+class Pair {
+public:
+	Pair();
+	virtual ~Pair();
+};
+
+} /* namespace prism */
+
+#endif /* PRISM_PAIR_H_ */
---
+++ src/prism/Pair.cpp
@@ -0,0 +1,22 @@
+/*
+ * Pair.cpp
+ * v1
+ *
+ *  Created on: Aug 20, 2016
+ *      Author: iainhemstock
+ */
+
+#include <prism/Pair.h>
+
+namespace prism {
+
+Pair::Pair() {
+
+
+}
+
+Pair::~Pair() {
+
+}
+
+} /* namespace prism */
