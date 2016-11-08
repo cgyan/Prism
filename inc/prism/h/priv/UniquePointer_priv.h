3e2554c3f9525a43c7dfe343fbda8ce93c13b619
iainhemstock <iainhemstock@googlemail.com>
2016-11-08 10:54:25 +0000

    created UniquePointer_priv.h

---
+++ inc/prism/h/priv/UniquePointer_priv.h
@@ -0,0 +1,16 @@
+/*
+ * UniquePointer_priv.h
+ * v1
+ *
+ *  Created on: Nov 8, 2016
+ *      Author: iainhemstock
+ */
+
+#ifndef PRISM_UNIQUE_POINTER_PRIV_H_
+#define PRISM_UNIQUE_POINTER_PRIV_H_
+
+
+
+
+
+#endif /* PRISM_UNIQUE_POINTER_PRIV_H_ */
