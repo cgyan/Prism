c62916ea295a564725379a4eeebda5b00328c76a
iainhemstock <iainhemstock@googlemail.com>
2016-12-29 22:57:42 +0000

    end of day commit

--- inc/prism/h/PVector.h
+++ inc/prism/h/PVector.h
@@ -12,85 +12,96 @@
 #include <prism/h/global.h>
 #include <prism/algorithm>
 #include <prism/OutOfBoundsException>
+#include <prism/Allocator>
 
 PRISM_BEGIN_NAMESPACE
 
 class PVector {
 public:
-	int*		_m_start;
-	int*		_m_end;
-	int*		_m_finish;
-	/*
-	 *
-	 */
+	int*			_m_start;
+	int*			_m_end;
+	int*			_m_finish;
+
 	PVector()
 	: _m_start(nullptr),
 	  _m_end(nullptr),
 	  _m_finish(nullptr)
 	{}
 
-	/*
-	 *
-	 */
+	PVector(const int size, const int value=int()) {
+		_m_start = _m_allocate(size);
+		_m_end = _m_start + size;
+		_m_finish = _m_start + size;
+		prism::uninitialized_fill_n(_m_start, size, value);
+	}
+
 	~PVector() {
 		delete [] _m_start;
 	}
 
+	void
+	append(const int value) {
+		insert(size(), 1, value);
+	}
+
 	int&
 	at(const int index) {
-		if (outOfBounds(index))
+		if (_m_outOfBounds(index))
 			throw prism::OutOfBoundsException(index);
 		return _m_start[index];
 	}
 
-	/*
-	 *
-	 */
 	const int
 	capacity() {
 		return _m_finish - _m_start;
 	}
-	/*
-	 *
-	 */
+
 	int *
 	data() {
 		return _m_start;
 	}
-	/*
-	 *
-	 */
+
 	const bool
 	empty() {
 		return size() == 0;
 	}
-	/*
-	 *
-	 */
-	void
-	insert(const int value) {
-		if (size() + 1 > capacity())
-			reserve(capacity() * 2 + 1);
 
-		_m_start[size()] = value;
-		_m_end += 1;
+	void
+	insert(const int index, const int value) {
+		insert(index, 1, value);
 	}
-	/*
-	 *
-	 */
+
+	void
+	insert(const int index, const int count, const int value) {
+		if (_m_invalidInsertionPoint(index))
+			throw prism::OutOfBoundsException(index);
+		if (size() + count > capacity())
+			reserve(capacity() * 2 + count);
+
+		prism::copy_backward(_m_start+index, _m_end, _m_end+count);
+		prism::fill(_m_start+index, _m_start+index+count, value);
+		_m_increaseSizeBy(count);
+	}
+
+	void
+	replace(const int index, const int value) {
+		if (_m_outOfBounds(index))
+			throw prism::OutOfBoundsException(index);
+		_m_replaceValueAtIndex(index, value);
+	}
+
 	void
 	reserve(const int size) {
 		int newCapacity = size;
 
-		throwIfNewCapacityIsNegative(newCapacity);
+		if(_m_isNegative(newCapacity))
+			throw std::bad_alloc();
 
 		if (newCapacity > capacity()) {
-			reallocateAndCopy(newCapacity);
+			_m_reallocateAndCopy(newCapacity);
 		}
 	}
-	/*
-	 *
-	 */
+
 	const int
 	size() {
 		return _m_end - _m_start;
@@ -103,14 +114,14 @@ public:
 
 private:
 	int*
-	allocate(int newCapacity) {
+	_m_allocate(int newCapacity) {
 		return new int[newCapacity];
 	}
 
 	void
-	reallocateAndCopy(int newCapacity) {
+	_m_reallocateAndCopy(int newCapacity) {
 		int oldSize = size();
-		int* newStorage = allocate(newCapacity);
+		int* newStorage = _m_allocate(newCapacity);
 		prism::copy(_m_start, _m_start + oldSize, newStorage);
 		delete [] _m_start;
 		_m_start = newStorage;
@@ -118,15 +129,28 @@ private:
 		_m_finish = _m_start + newCapacity;
 	}
 
-	void
-	throwIfNewCapacityIsNegative(int newCapacity) {
-		if (newCapacity < 0)
-			throw std::bad_alloc();
+	const bool
+	_m_isNegative(int num) {
+		return num < 0;
 	}
 
 	const bool
-	outOfBounds(const int index) {
+	_m_outOfBounds(const int index) {
 		return index < 0 || index >= size();
+	}
+
+	int
+	_m_replaceValueAtIndex(const int index, const int value) {
+		return _m_start[index] = value;
+	}
+
+	bool
+	_m_invalidInsertionPoint(const int index) {
+		return index < 0 || index > size();
+	}
+
+	void _m_increaseSizeBy(const int amount) {
+		_m_end += amount;
 	}
 };
 
