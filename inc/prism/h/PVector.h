cf9a945d355a064cf8b01377ab648308bfcaac24
iainhemstock <iainhemstock@googlemail.com>
2016-12-30 18:38:15 +0000

    committing

--- inc/prism/h/PVector.h
+++ inc/prism/h/PVector.h
@@ -13,14 +13,20 @@
 #include <prism/algorithm>
 #include <prism/OutOfBoundsException>
 #include <prism/Allocator>
+#include <prism/type_traits>
+#include <prism/Iterator>
+#include <initializer_list>
 
 PRISM_BEGIN_NAMESPACE
 
 class PVector {
 public:
-	int*			_m_start;
-	int*			_m_end;
-	int*			_m_finish;
+	using iterator 			= prism::SequenceIterator<int, false>;
+	using const_iterator 	= prism::SequenceIterator<int, true>;
+
+	int*	_m_start;
+	int*	_m_end;
+	int*	_m_finish;
 
 	PVector()
 	: _m_start(nullptr),
@@ -28,11 +34,29 @@ public:
 	  _m_finish(nullptr)
 	{}
 
-	PVector(const int size, const int value=int()) {
-		_m_start = _m_allocate(size);
-		_m_end = _m_start + size;
-		_m_finish = _m_start + size;
-		prism::uninitialized_fill_n(_m_start, size, value);
+	PVector(const int size, const int value=int())
+	: _m_start(nullptr),
+	  _m_end(nullptr),
+	  _m_finish(nullptr)
+	{
+		_m_fillInitialize(size, value);
+	}
+
+	template <typename ForwardIterator>
+	PVector(ForwardIterator first, ForwardIterator last)
+	: _m_start(nullptr),
+	  _m_end(nullptr),
+	  _m_finish(nullptr)
+	{
+		_m_rangeInitialize(first, last);
+	}
+
+	PVector(const std::initializer_list<int> il)
+	: _m_start(nullptr),
+	  _m_end(nullptr),
+	  _m_finish(nullptr)
+	{
+		_m_rangeInitialize(il.begin(), il.end());
 	}
 
 	~PVector() {
@@ -51,9 +75,24 @@ public:
 		return _m_start[index];
 	}
 
+	iterator
+	begin() {
+		return iterator(_m_start);
+	}
+
 	const int
-	capacity() {
+	capacity() const {
 		return _m_finish - _m_start;
+	}
+
+	const_iterator
+	cbegin() {
+		return const_iterator(_m_start);
+	}
+
+	const_iterator
+	cend() {
+		return const_iterator(_m_end);
 	}
 
 	int *
@@ -62,8 +101,13 @@ public:
 	}
 
 	const bool
-	empty() {
+	empty() const {
 		return size() == 0;
+	}
+
+	iterator
+	end() {
+		return iterator(_m_end);
 	}
 
 	void
@@ -80,7 +124,47 @@ public:
 
 		prism::copy_backward(_m_start+index, _m_end, _m_end+count);
 		prism::fill(_m_start+index, _m_start+index+count, value);
-		_m_increaseSizeBy(count);
+		_m_increaseSizeeBy(count);
+	}
+
+	void
+	prepend(const int value) {
+		insert(0, 1, value);
+	}
+
+	void
+	remove(const int index) {
+		remove(index, 1);
+	}
+
+	void
+	remove(const int index, const int count) {
+		if (_m_outOfBounds(index))
+			throw prism::OutOfBoundsException(index);
+
+		prism::copy(_m_start+index+count, _m_end, _m_start+index);
+		_m_decreaseSizeBy(count);
+	}
+
+	void
+	removeAll(const int value) {
+		_m_end = prism::remove(_m_start, _m_end, value);
+	}
+
+	void
+	removeFirst() {
+		remove(0);
+	}
+
+	template <typename Predicate>
+	void
+	removeIf(Predicate pred) {
+		_m_end = prism::remove_if(_m_start, _m_end, pred);
+	}
+
+	void
+	removeLast() {
+		remove(size()-1);
 	}
 
 	void
@@ -103,7 +187,7 @@ public:
 	}
 
 	const int
-	size() {
+	size() const {
 		return _m_end - _m_start;
 	}
 
@@ -130,12 +214,12 @@ private:
 	}
 
 	const bool
-	_m_isNegative(int num) {
+	_m_isNegative(int num) const {
 		return num < 0;
 	}
 
 	const bool
-	_m_outOfBounds(const int index) {
+	_m_outOfBounds(const int index) const {
 		return index < 0 || index >= size();
 	}
 
@@ -145,12 +229,34 @@ private:
 	}
 
 	bool
-	_m_invalidInsertionPoint(const int index) {
+	_m_invalidInsertionPoint(const int index) const {
 		return index < 0 || index > size();
 	}
 
-	void _m_increaseSizeBy(const int amount) {
+	void _m_increaseSizeeBy(const int amount) {
 		_m_end += amount;
+	}
+
+	void
+	_m_decreaseSizeBy(const int amount) {
+		_m_end -= amount;
+	}
+
+	void
+	_m_fillInitialize(const int size, const int value) {
+		_m_start = _m_allocate(size);
+		_m_end = _m_start + size;
+		_m_finish = _m_start + size;
+		prism::uninitialized_fill_n(_m_start, size, value);
+	}
+
+	template <typename ForwardIterator>
+	void
+	_m_rangeInitialize(ForwardIterator first, ForwardIterator last) {
+		_m_start = _m_allocate(last-first);
+		_m_end = _m_start + (last-first);
+		_m_finish = _m_start + (last-first);
+		prism::uninitialized_copy(first, last, _m_start);
 	}
 };
 
