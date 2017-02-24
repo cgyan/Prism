dff0dfb5b548af13ca1ce32568fdb42641e4840e
iainhemstock <iainhemstock@googlemail.com>
2017-02-24 18:40:51 +0000

    added public types

--- inc/prism/h/PList.h
+++ inc/prism/h/PList.h
@@ -10,10 +10,27 @@
 #define PRISM_PLIST_H_
 
 #include <prism/global>
+#include <prism/Allocator>
 
 PRISM_BEGIN_NAMESPACE
 
+template <typename T, typename Allocator = prism::Allocator<T>>
+class PList {
+private:
+	using vt_alloc_traits 	= prism::AllocatorTraits<Allocator>;
+public:
+	using allocator_type 	= Allocator;
+	using value_type 		= typename vt_alloc_traits::value_type;
+	using reference 		= typename vt_alloc_traits::reference;
+	using const_reference 	= typename vt_alloc_traits::const_reference;
+	using pointer 			= typename vt_alloc_traits::pointer;
+	using const_pointer 	= typename vt_alloc_traits::const_pointer;
+	using difference_type 	= typename vt_alloc_traits::difference_type;
+	using size_type 		= typename vt_alloc_traits::size_type;
+public:
 
+
+};
 
 PRISM_END_NAMESPACE
 
