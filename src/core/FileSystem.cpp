da8c3bdd6da396460e6e05167c78f668be5e2f20
iainhemstock <iainhemstock@googlemail.com>
2018-03-27 13:15:40 +0100

    delete FileSystem::create implementation

--- src/core/FileSystem.cpp
+++ src/core/FileSystem.cpp
@@ -5,13 +5,6 @@
 
 PRISM_BEGIN_NAMESPACE
 
-AbstractFileSystem *
-FileSystem::create()
-{
-        static FileSystem instance;
-        return &instance;
-}
-
 void
 FileSystem::createFile(const std::string& filename)
 {}
