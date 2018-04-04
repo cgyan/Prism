0631f82f9171e40e29347befb2e9d8c3ade1f692
iainhemstock <iainhemstock@googlemail.com>
2018-04-04 14:15:56 +0100

    Move logic into impl class

--- src/core/FileInfo.cpp
+++ src/core/FileInfo.cpp
@@ -22,6 +22,8 @@ convertPathToUnixSeparators(const std::string& path) {
 class FileInfoInternal
 {
 public:
+        enum class Property { Filename, Basename, Suffix, EntireSuffix, EntireBasename, AbsolutePath, CanonicalFilePath };
+public:
         FileInfoInternal(const std::string& file, std::shared_ptr<AbstractFileSystem> fileSystem);
 
         Vector<std::string> split(const std::string& filePath, const char delim) const;
@@ -31,11 +33,35 @@ public:
         const std::string file() const;
         const bool fileExists() const;
         const int fileSize() const;
+        const std::string fileProperty(Property property) const;
+private:
         const std::string absolutePath() const;
+        const std::string filename() const;
+        const std::string basename() const;
+        const std::string suffix() const;
+        const std::string entireSuffix() const;
+        const std::string entireBasename() const;
+        const std::string canonicalFilePath() const;
+
 private:
         std::shared_ptr<AbstractFileSystem> m_fileSystem{nullptr};
         std::string m_file{""};
 };
+
+const std::string
+FileInfoInternal::fileProperty(FileInfoInternal::Property property) const
+{
+        switch (property)
+        {
+                case Property::Filename: return filename(); break;
+                case Property::Basename: return basename(); break;
+                case Property::Suffix: return suffix(); break;
+                case Property::EntireSuffix: return entireSuffix(); break;
+                case Property::EntireBasename: return entireBasename(); break;
+                case Property::AbsolutePath: return absolutePath(); break;
+                case Property::CanonicalFilePath: return canonicalFilePath(); break;
+        }
+}
 
 FileInfoInternal::FileInfoInternal(const std::string& file, std::shared_ptr<AbstractFileSystem> fileSystem)
 : m_file{convertPathToUnixSeparators(file)},
@@ -111,6 +137,54 @@ FileInfoInternal::absolutePath() const
 {
         return m_fileSystem->absolutePath(m_file.c_str());
 }
+
+const std::string
+FileInfoInternal::filename() const
+{
+        size_t pos = m_file.find_last_of("/");
+        return m_file.substr(pos+1);
+}
+
+const std::string
+FileInfoInternal::basename() const
+{
+        std::string ret = filename();
+        const size_t pos = ret.find_first_of(".");
+        return ret.substr(0, pos);
+}
+
+const std::string
+FileInfoInternal::suffix() const
+{
+        const size_t pos = m_file.find_last_of(".");
+        if (pos == std::string::npos) return "";
+        return m_file.substr(pos+1);
+}
+
+const std::string
+FileInfoInternal::entireSuffix() const
+{
+        std::string ret = filename();
+        const size_t pos = ret.find_first_of(".");
+        if (pos == std::string::npos) return "";
+        return ret.substr(pos+1);
+}
+
+const std::string
+FileInfoInternal::entireBasename() const
+{
+        std::string ret = filename();
+        const size_t pos = ret.find_last_of(".");
+        return ret.substr(0, pos);
+}
+
+const std::string
+FileInfoInternal::canonicalFilePath() const
+{
+        Vector<std::string> tokens = split(m_file, '/');
+        Stack<std::string> stack = removeDotAndDoubleDotComponents(&tokens);
+        return buildCanonicalString(stack);
+}
 //======================================================================================================================
 //
 //======================================================================================================================
@@ -149,56 +223,44 @@ FileInfo::size() const
 const std::string
 FileInfo::filename() const
 {
-        size_t pos = m_impl->file().find_last_of("/");
-        return m_impl->file().substr(pos+1);
+        return m_impl->fileProperty(FileInfoInternal::Property::Filename);
 }
 
 const std::string
 FileInfo::basename() const
 {
-        std::string ret = filename();
-        const size_t pos = ret.find_first_of(".");
-        return ret.substr(0, pos);
+        return m_impl->fileProperty(FileInfoInternal::Property::Basename);
 }
 
 const std::string
 FileInfo::suffix() const
 {
-        const size_t pos = m_impl->file().find_last_of(".");
-        if (pos == std::string::npos) return "";
-        return m_impl->file().substr(pos+1);
+        return m_impl->fileProperty(FileInfoInternal::Property::Suffix);
 }
 
 const std::string
 FileInfo::entireSuffix() const
 {
-        std::string ret = filename();
-        const size_t pos = ret.find_first_of(".");
-        if (pos == std::string::npos) return "";
-        return ret.substr(pos+1);
+        return m_impl->fileProperty(FileInfoInternal::Property::EntireSuffix);
 }
 
 const std::string
 FileInfo::entireBasename() const
 {
-        std::string ret = filename();
-        const size_t pos = ret.find_last_of(".");
-        return ret.substr(0, pos);
+        return m_impl->fileProperty(FileInfoInternal::Property::EntireBasename);
 }
 
 const std::string
 FileInfo::absolutePath() const
 {
         if (m_impl->file() == "") return std::string{};
-        return m_impl->absolutePath();
+        return m_impl->fileProperty(FileInfoInternal::Property::AbsolutePath);
 }
 
 const std::string
 FileInfo::canonicalFilePath() const
 {
-        Vector<std::string> tokens = m_impl->split(m_impl->file(), '/');
-        Stack<std::string> stack = m_impl->removeDotAndDoubleDotComponents(&tokens);
-        return m_impl->buildCanonicalString(stack);
+        return m_impl->fileProperty(FileInfoInternal::Property::CanonicalFilePath);
 }
 
 PRISM_END_NAMESPACE
